24f16f246d0aa87c80fc75972b58ba58a04a9e86
Stanko Yanev <stanko.yanev@edu.mon.bg>
2025-05-12 11:47:28 +0300

    Add project files.

---
+++ BranchExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 3;
+            Console.Write(a + b);
+        }
+    }
+}
